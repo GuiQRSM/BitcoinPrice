10ae830b653467a8d455bc5d3f5ec7976517ccb6
GuiQRSM <endbeob15@gmail.com>
2021-12-13 15:09:56 -0300

    Implement in the 'lib' folder the dart file 'Main_Action' already configured with the 'import 'package:flutter/material.dart' and with the called 'stful' structure.

---
+++ lib/Main_Action.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MainAction extends StatefulWidget {
+  const MainAction({Key? key}) : super(key: key);
+
+  @override
+  _MainActionState createState() => _MainActionState();
+}
+
+class _MainActionState extends State<MainAction> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
