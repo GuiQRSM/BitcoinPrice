ceace3f43d00f33e8c0aa897f6b9704518182a8a
GuiQRSM <endbeob15@gmail.com>
2021-12-13 15:54:27 -0300

    Implements in 'children: <>[ ]' of class 'Column()' which receives as 'child:,' the class 'Text()' which contains the variable '_dinamicPrice'

--- lib/Main_Action.dart
+++ lib/Main_Action.dart
@@ -3,12 +3,13 @@ import 'package:flutter/material.dart';
 
 class MainAction extends StatefulWidget {
 
-  var mainColor = Color.fromRGBO(51, 255, 173, 1);
-  var extraColor = Color.fromRGBO(0, 119, 179, 1);
-
   @override
   _MainActionState createState() => _MainActionState();
 }
+
+var mainColor = Color.fromRGBO(51, 255, 173, 1);
+var extraColor = Color.fromRGBO(0, 119, 179, 1);
+var _dinamicPrice = "VALUE";
 
 class _MainActionState extends State<MainAction> {
   @override
@@ -22,10 +23,21 @@ class _MainActionState extends State<MainAction> {
           child: Column(
             children: <Widget>[
               Padding(
-                  padding: EdgeInsets.only(top:160,bottom: 150),
+                  padding: EdgeInsets.only(top:160,bottom: 4),
                 child: Image.asset(
                     "img/bitcoin_edit.png",
                   height: 100,
+                ),
+              ),
+              Padding(
+                  padding: EdgeInsets.only(top: 10),
+                child: Text(
+                  "$_dinamicPrice",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.w600,
+                    color: extraColor,
+                  ),
                 ),
               )
             ],
